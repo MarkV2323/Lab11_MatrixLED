4bce5e856f5836c2fc760f4b441a6c94ba2ff900
Mark Vincent <41304553+MarkV2323@users.noreply.github.com>
2021-07-18 19:43:31 -0700

    added init dev files

--- source/main.c
+++ source/main.c
@@ -1,8 +1,12 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+/*	Author: Mark Alan Vincent II
+ *  Partner(s) Name: NA
+ *	Lab Section: A01
+ *	Assignment: Lab #11  Exercise #1
+ *	Youtube:
+ *  Exercise Description:
+ *
+ *
+ *
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -10,14 +14,87 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "timer.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+// Method for handling writing to register.
+void transmit_data(unsigned char data) {
+    int i;
+    for (i = 0; i < 8 ; ++i) {
+   	 // Sets SRCLR to 1 allowing data to be set
+   	 // Also clears SRCLK in preparation of sending data
+   	 PORTC = 0x08;
+   	 // set SER = next bit of data to be sent.
+   	 PORTC |= ((data >> i) & 0x01);
+   	 // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+   	 PORTC |= 0x02;
+    }
+    // set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
+    PORTC |= 0x04;
+    // clears all lines in preparation of a new transmission
+    PORTC = 0x00;
+}
 
-    /* Insert your solution below */
-    while (1) {
+// globals (shared varibles for the SMs)
+unsigned char tmpA = 0x00;
+
+
+// Our concurrent synchSMs
+enum SM1_States { SM1_Start } sm1_state;
+int TickFct_BlinkLED(int state) {
+
+    // Read Input from tmpA
+    tmpA = ~PINA & 0xFF;
+
+    // Transitions
+    switch(state) {
+
+        case SM1_Start:
+            break;
+
+        default:
+            // Default
+            state = SM1_Start;
+            break;
 
     }
+
+    // Actions
+    switch(state) {
+
+        case SM1_Start:
+            break;
+
+        default:
+            break;
+
+    }
+
+    // End of Tick Function SM1
+    return state;
+}
+
+// Main Funcntion
+int main(void) {
+
+    // PORTS
+    DDRA = 0x00; PORTA = 0xFF; // PortA as input
+    DDRB = 0xFF; PORTB = 0x00; // PortB as output
+    DDRC = 0xFF; PORTC = 0x00; // PortC as output
+
+    // Setup Task List
+    unsigned char i = 0;
+
+    // SM1
+    //tasks[i].state = SM1_Start;
+	  //tasks[i].period = 1500;
+	  //tasks[i].elapsedTime = tasks[i].period;
+	  //tasks[i].TickFct = &TickFct_BlinkLED;
+	  //i++;
+
+    // Setup System Period & Timer to ON.
+    TimerSet(500);
+    TimerOn();
+    while (1) {}
     return 1;
 }
